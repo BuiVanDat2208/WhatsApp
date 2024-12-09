1f168b197caaa7223743cabb2c2ec36fe24b9880
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-09 10:23:41 +0700

    Covert language

---
+++ lib/feature/welcome/widget/language_button.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/utils/coloors.dart';
+
+class LanguageButton extends StatelessWidget {
+  const LanguageButton({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: const Color(0xFF182229),
+      borderRadius: BorderRadius.circular(20),
+      child: InkWell(
+        onTap: () {},
+        borderRadius: BorderRadius.circular(20),
+        highlightColor: const Color(0xFF09141A),
+        child: const Padding(
+          padding: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+          child: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Icon(
+                Icons.language_outlined,
+                color: Coloors.greenDark,
+              ),
+              SizedBox(width: 10),
+              Text(
+                "English",
+                style: TextStyle(color: Coloors.greyDark),
+              ),
+              SizedBox(width: 10),
+              Icon(
+                Icons.keyboard_arrow_down_outlined,
+                color: Coloors.greenDark,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
