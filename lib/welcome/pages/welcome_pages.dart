ff01945150871c611893cd5be3ac651a0e62003c
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-09 08:45:09 +0700

    Update

---
+++ lib/welcome/pages/welcome_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomePages extends StatefulWidget {
+  const WelcomePages({super.key});
+
+  @override
+  State<WelcomePages> createState() => _WelcomePagesState();
+}
+
+class _WelcomePagesState extends State<WelcomePages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
