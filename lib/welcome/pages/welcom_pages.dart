e35bc22f78eb25884db769fa02b704c85271d099
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-09 08:20:55 +0700

    Build welcome

---
+++ lib/welcome/pages/welcom_pages.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcomPages extends StatefulWidget {
+  const WelcomPages({super.key});
+
+  @override
+  State<WelcomPages> createState() => _WelcomPagesState();
+}
+
+class _WelcomPagesState extends State<WelcomPages> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
