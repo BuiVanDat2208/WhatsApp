969a53d884790d52bdd58ac8eda746fe936188e9
BuiVanDat2208 <buivandat2003hn@gmail.comm>
2024-12-09 15:56:33 +0700

    Show Alert Dialog

---
+++ lib/common/helper/show_alert_dialog.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/common/extension/custom_theme_extension.dart';
+import 'package:whatsapp_clone/common/utils/coloors.dart';
+
+showAlertDialog({
+  required BuildContext context,
+  required String message,
+  String? btnText,
+}) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        content: Text(
+          message,
+          style: TextStyle(
+            color: context.theme.blueColor,
+            fontSize: 15,
+          ),
+        ),
+        contentPadding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        actions: [
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: Text(
+              btnText ?? "OK",
+              style: TextStyle(
+                color: context.theme.circleImageColor,
+              ),
+            ),
+          ),
+        ],
+      );
+    },
+  );
+}
